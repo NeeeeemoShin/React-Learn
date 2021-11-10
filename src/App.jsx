54ce4b83e69080b446b20606fc4d84d300234cc6
NeeeeemoShin <92913474+NeeeeemoShin@users.noreply.github.com>
2021-11-10 10:54:59 +0900

    CSS装飾-InlineStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { useState, useCallback } from "react";
-import { ChildArea } from "./ChildArea";
+// import { useState, useCallback } from "react";
+// import { ChildArea } from "./ChildArea";
+import { InlineStyle } from "./components/InlineStyle";
 import "./styles.css";
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
+  // const [text, setText] = useState("");
+  // const [open, setOpen] = useState(false);
 
-  const onChangeText = (event) => {
-    setText(event.target.value);
-  };
-  const onClickOpen = () => setOpen(!open);
-  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+  // const onChangeText = (event) => {
+  //   setText(event.target.value);
+  // };
+  // const onClickOpen = () => setOpen(!open);
+  // const onClickClose = useCallback(() => setOpen(false), [setOpen]);
 
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText} />
+      {/* <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
+      <ChildArea open={open} onClickClose={onClickClose} /> */}
+      <InlineStyle />
     </div>
   );
 }
